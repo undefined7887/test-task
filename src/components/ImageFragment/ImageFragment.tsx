188576cecf888e89ce051709c3e9d993af055275
undefined7887 <undefined7887@gmail.com>
2020-12-15 14:31:04 +0300

    [FEATURE] Implemented scrolls

--- src/components/ImageFragment/ImageFragment.tsx
+++ src/components/ImageFragment/ImageFragment.tsx
@@ -3,7 +3,7 @@ import Konva from "konva";
 import {
   Stage,
   Layer,
-  Image
+  Image, Rect
 } from "react-konva";
 import useImage from "use-image";
 
@@ -21,133 +21,252 @@ function p(x: number, y: number) {
 }
 
 const SCALE_COEFFICIENT = 1.1
+const CONTAINER_HEIGHT = 450
+const CONTAINER_WIDTH = 800
+const SCROLL_PADDING = 10
+
+// That should be equal
+const SCROLL_HEIGHT = 10
+const SCROLL_WIDTH = 10
 
 export default function ImageFragment() {
   const [image] = useImage(ImageSrc)
-  const containerRef = useRef<HTMLDivElement>()
-  const stageRef = useRef<Konva.Stage>();
+  const stageRef = useRef<Konva.Stage>()
+  const imageRef = useRef<Konva.Image>()
+  const horizontalScrollRef = useRef<Konva.Rect>()
+  const verticalScrollRef = useRef<Konva.Rect>()
 
-  function redrawImage(zoom: boolean, coefficient: number, point: Point) {
-    let stage = stageRef.current;
+  function scaleImage(zoom: boolean, point: Point) {
+    let stage = stageRef.current
+    let image = imageRef.current
 
-    let width = containerRef.current.clientWidth;
-    let height = containerRef.current.clientHeight;
+    let scale = image.scaleX()
+    let pointerPosition = point
 
-    let scale = stage.scaleX();
-    let pointerPosition = point;
-
-    let stageToPointerVector = p(
-      (pointerPosition.x - stage.x()) / scale,
-      (pointerPosition.y - stage.y()) / scale,
+    let imageToPointerVector = p(
+      (pointerPosition.x - image.x()) / scale,
+      (pointerPosition.y - image.y()) / scale,
     );
 
     let newScale = zoom
-      ? scale * coefficient
-      : scale / coefficient;
+      ? scale * SCALE_COEFFICIENT
+      : scale / SCALE_COEFFICIENT
 
-    let newStagePosition = p(
-      pointerPosition.x - stageToPointerVector.x * newScale,
-      pointerPosition.y - stageToPointerVector.y * newScale,
+    let newImagePosition = p(
+      pointerPosition.x - imageToPointerVector.x * newScale,
+      pointerPosition.y - imageToPointerVector.y * newScale,
     );
 
     if (newScale <= 1) {
       newScale = 1
-      newStagePosition = p(0, 0)
+      newImagePosition = p(0, 0)
     } else {
-      newStagePosition = imagePositionGuard(
-        p(width, height),
+      newImagePosition = imagePositionGuard(
         newScale,
-        newStagePosition
+        newImagePosition,
+        p(stage.width(), stage.height())
       )
     }
 
     // Redrawing
-    stage.scale(p(newScale, newScale));
-    stage.position(newStagePosition);
+    image.scale(p(newScale, newScale))
+    image.position(newImagePosition)
 
-    console.log("redraw", newScale, newStagePosition)
+    // Redrawing scrolls
+    redrawScrolls()
 
     stage.batchDraw();
   }
 
-  function imagePositionGuard(size: Point, scale: number, stagePosition: Point): Point {
-    let leftTop = p(
-      stagePosition.x,
-      stagePosition.y
-    )
+  function imagePositionGuard(scale: number, position: Point, size: Point): Point {
+    let leftTop = position
     let rightTop = p(
-      stagePosition.x + size.x * scale,
-      stagePosition.y
+      position.x + size.x * scale,
+      position.y
     )
 
     let leftBottom = p(
-      stagePosition.x,
-      stagePosition.y + size.y * scale
+      position.x,
+      position.y + size.y * scale
     )
     let rightBottom = p(
-      stagePosition.x + size.x * scale,
-      stagePosition.y + size.y * scale
+      position.x + size.x * scale,
+      position.y + size.y * scale
     )
 
     if (leftTop.x > 0 || leftBottom.x > 0)
-      stagePosition.x = 0
+      position.x = 0
 
     if (leftTop.y > 0 || rightTop.y > 0)
-      stagePosition.y = 0
+      position.y = 0
 
     if (rightTop.x < size.x || rightBottom.x < size.x)
-      stagePosition.x = size.x - size.x * scale
+      position.x = size.x - size.x * scale
 
     if (leftBottom.y < size.y || rightBottom.y < size.y)
-      stagePosition.y = size.y - size.y * scale
+      position.y = size.y - size.y * scale
 
-    return stagePosition
+    return position
+  }
+
+  function redrawScrolls() {
+    let stage = stageRef.current
+    let image = imageRef.current
+    let horizontalScroll = horizontalScrollRef.current
+    let verticalScroll = verticalScrollRef.current
+
+    let imageScale = image.scaleX();
+    let imagePosition = image.position()
+
+    let horizontalCoefficient = stage.width() / (image.width() * imageScale)
+    let verticalCoefficient = stage.height() / (image.height() * imageScale)
+
+    if (horizontalCoefficient == 1)
+      horizontalCoefficient = 0
+
+    if (verticalCoefficient == 1)
+      verticalCoefficient = 0
+
+    let horizontalWidth = Math.max(horizontalCoefficient * stage.width() - SCROLL_PADDING * 2, 0)
+    let horizontalX = Math.abs(imagePosition.x / imageScale) + SCROLL_PADDING
+
+    let verticalHeight = Math.max(verticalCoefficient * stage.height() - SCROLL_PADDING * 2, 0)
+    let verticalY = Math.abs(imagePosition.y / imageScale) + SCROLL_PADDING
+
+    // Redrawing horizontal scroll
+    horizontalScroll.width(horizontalWidth)
+    horizontalScroll.x(horizontalX)
+
+    // Redrawing vertical scroll
+    verticalScroll.height(verticalHeight)
+    verticalScroll.y(verticalY)
+
+    stage.batchDraw()
   }
 
   function onWheel(e) {
-    redrawImage(
+    let stage = stageRef.current
+
+    scaleImage(
       e.evt.deltaY < 0,
-      SCALE_COEFFICIENT,
-      stageRef.current.getPointerPosition()
+      stage.getPointerPosition()
     )
   }
 
   function onScaleInButtonClick() {
-    redrawImage(
+    let stage = stageRef.current
+
+    scaleImage(
       true,
-      SCALE_COEFFICIENT,
-      {
-        x: containerRef.current.clientWidth / 2,
-        y: containerRef.current.clientHeight / 2
-      }
+      p(stage.width() / 2, stage.height() / 2)
     )
   }
 
   function onScaleOutButtonClick() {
-    redrawImage(
+    scaleImage(
       false,
-      SCALE_COEFFICIENT,
-      {
-        x: containerRef.current.clientWidth / 2,
-        y: containerRef.current.clientHeight / 2
-      }
+      p(stageRef.current.width() / 2, stageRef.current.height() / 2)
     )
+  }
+
+  function moveImage(delta: Point) {
+    let stage = stageRef.current
+    let image = imageRef.current
+
+    let scale = image.scaleX()
+
+    let newPosition = p(
+      -(image.width() * scale - stage.width()) * delta.x,
+      -(image.height() * scale - stage.height()) * delta.y
+    )
+
+    // Redrawing image
+    image.position(newPosition)
+
+    stage.batchDraw()
+  }
+
+  function onScroll() {
+    let stage = stageRef.current
+    let horizontalScroll = horizontalScrollRef.current
+    let verticalScroll = verticalScrollRef.current
+
+    let totalWidth = stage.width() - SCROLL_PADDING * 2 - horizontalScroll.width()
+    let totalHeight = stage.height() - SCROLL_PADDING * 2 - verticalScroll.height()
+
+    let deltaX = (horizontalScroll.x() - SCROLL_PADDING) / totalWidth
+    let deltaY = (verticalScroll.y() - SCROLL_PADDING) / totalHeight
+
+    // Redrawing image
+    moveImage(p(deltaX, deltaY))
+
+    stage.batchDraw()
+  }
+
+  function horizontalScrollDragBound(position: Point) {
+    let stage = stageRef.current
+    let scroll = horizontalScrollRef.current
+
+    position.x = Math.max(
+      Math.min(position.x, stage.width() - scroll.width() - SCROLL_PADDING),
+      SCROLL_PADDING
+    )
+    position.y = stage.height() - SCROLL_PADDING - SCROLL_HEIGHT
+
+    return position
+  }
+
+  function verticalScrollDragBound(position: Point) {
+    let stage = stageRef.current
+    let scroll = verticalScrollRef.current
+
+    position.x = stage.width() - SCROLL_PADDING - SCROLL_WIDTH
+    position.y = Math.max(
+      Math.min(position.y, stage.height() - scroll.height() - SCROLL_PADDING),
+      SCROLL_PADDING
+    )
+
+    return position
   }
 
   return (
     <div className={Styles.Fragment}>
-      <div ref={containerRef}
-           className={Styles.Container}>
-        <Stage ref={stageRef} className={Styles.Stage}
-               onWheel={onWheel}
-               width={800}
-               height={450}>
-          <Layer>
-            <Image image={image}
-                   width={800}
-                   height={450}/>
-          </Layer>
-        </Stage>
+      <div className={Styles.Container}>
+        <div className={Styles.StageWrap}>
+          <Stage ref={stageRef} className={Styles.Stage}
+                 onWheel={onWheel}
+                 width={CONTAINER_WIDTH}
+                 height={CONTAINER_HEIGHT}>
+            <Layer>
+              <Image ref={imageRef}
+                     image={image}
+                     width={CONTAINER_WIDTH}
+                     height={CONTAINER_HEIGHT}/>
+
+              <Rect ref={horizontalScrollRef}
+                    x={SCROLL_PADDING}
+                    y={CONTAINER_HEIGHT - SCROLL_PADDING - SCROLL_HEIGHT}
+                    height={SCROLL_HEIGHT}
+                    cornerRadius={SCROLL_HEIGHT / 2}
+                    draggable={true}
+                    fill="white"
+                    opacity={0.5}
+                    onDragMove={onScroll}
+                    dragBoundFunc={horizontalScrollDragBound}/>
+
+              <Rect ref={verticalScrollRef}
+                    x={CONTAINER_WIDTH - SCROLL_PADDING - SCROLL_WIDTH}
+                    y={SCROLL_PADDING}
+                    width={SCROLL_WIDTH}
+                    cornerRadius={SCROLL_WIDTH / 2}
+                    draggable={true}
+                    fill="white"
+                    opacity={0.5}
+                    onDragMove={onScroll}
+                    dragBoundFunc={verticalScrollDragBound}/>
+            </Layer>
+          </Stage>
+        </div>
         <div className={Styles.Controllers}>
           <div className={classes("material-icons", Styles.Controller)}
                onClick={onScaleInButtonClick}>
